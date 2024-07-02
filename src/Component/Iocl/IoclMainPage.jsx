132bc66cd1882a468d5c2e86447fba1b58c58cfd
Jeffrey-Ragland <jeffreyragland3009@gmail.com>
2024-07-02 19:44:34 +0530

    layout responsiveness in progress

--- src/Component/Iocl/IoclMainPage.jsx
+++ src/Component/Iocl/IoclMainPage.jsx
@@ -1,50 +1,136 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import IoclSidebar from './IoclSidebar';
+import {
+  Chart as ChartJS,
+  BarElement, 
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
+import { LiaChartLineSolid } from "react-icons/lia";
+import ioclLogo from "../Assets/ioclRound.png";
+import xymaLogo from '../Assets/xyma.png';
+
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
 const IoclMainPage = () => {
+
+  // line chart data
+  const lineData = {
+    labels: ["10", "20", "30", "40","50"],
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [65, 59, 80, 81, 56],
+        borderColor: "rgb(75, 192, 192)",
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+      },
+      {
+        label: "Dataset 2",
+        data: [45, 49, 60, 71, 46],
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+      },
+      {
+        label: "Dataset 3",
+        data: [35, 39, 50, 61, 36],
+        borderColor: "rgb(54, 162, 235)",
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+      },
+    ],
+  };
+
+  const lineOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Sensor Temperature Measurement",
+      },
+    },
+  };
+
+  // bar chart data
+  const barData = {
+    labels: ["Sensor 1", "Sensor 2", "Sensor 3"],
+    datasets: [
+      {
+        label: "Temperature Data",
+        data: [73, 87, 56],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.6)",
+          "rgba(54, 162, 235, 0.6)",
+          "rgba(255, 206, 86, 0.6)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+   const barOptions = {
+     indexAxis: "y",
+     responsive: true,
+     maintainAspectRatio: false,
+     plugins: {
+       legend: {
+         position: "top",
+       },
+       title: {
+         display: true,
+         text: "Peak Analysis",
+       },
+     },
+   };
+
+
   return (
     <div className="flex h-screen">
       {/* sidebar */}
-      <div className="w-[15%] border border-black h-full">
+      <div className="lg:w-[15%] h-full">
         <IoclSidebar />
       </div>
 
       {/* main content */}
-      <div className="w-[85%] border border-black p-4 h-full">
+      <div className="w-full lg:w-[85%]  p-4 h-full">
         {/* header */}
-        <div className="flex justify-between h-[5%] border border-black ">
-          <div>Activity Status</div>
-          <div>IOCL Dashboard</div>
+        <div className="flex justify-between h-[5%] px-2">
+          <div className='hidden md:invisible'>Activity Status</div>
+          <div className='flex gap-2 md:hidden'>
+            <img src={ioclLogo} alt='iocl' />
+            <img src={xymaLogo} alt='xyma' />
+          </div>
+          <div className='font-semibold'>IOCL Dashboard</div>
           <Link to="/login">
             <span
-              className="border border-black p-1 bg-red-400"
+              className=" py-1 px-2 text-white font-medium text-sm rounded-md"
+              style={{
+                background: "linear-gradient(90deg, #f22213 0%, #f03f32 100%)",
+              }}
               onClick={() => {
                 localStorage.removeItem("Project");
                 localStorage.removeItem("token");
                 localStorage.removeItem("Controles");
               }}
             >
-              logout
+              Logout
             </span>
           </Link>
         </div>
 
         {/* content */}
-        <div className="flex h-[95%] border border-black ">
+        <div className="flex h-[95%]  ">
           {/* cards */}
-          <div className="w-[20%] border border-black flex flex-col gap-2 p-2 text-white">
+          <div className="w-[20%]  flex flex-col gap-2 p-2 text-white">
             <div
-              className="h-1/4 rounded-md"
-              style={{
-                background:
-                  "linear-gradient(90deg, rgba(2,22,79,255) 0%, #02306b 100%)",
-              }}
-            >
-              Last Update
-            </div>
-            <div
-              className="h-1/4 rounded-md "
+              className="h-1/4 rounded-md p-4"
               style={{
                 background:
                   "linear-gradient(90deg, rgba(2,22,79,255) 0%, #02306b 100%)",
@@ -53,7 +139,7 @@ const IoclMainPage = () => {
               Sensor 1
             </div>
             <div
-              className="h-1/4 rounded-md "
+              className="h-1/4 rounded-md p-4"
               style={{
                 background:
                   "linear-gradient(90deg, rgba(2,22,79,255) 0%, #02306b 100%)",
@@ -62,7 +148,7 @@ const IoclMainPage = () => {
               Sensor 2
             </div>
             <div
-              className="h-1/4 rounded-md "
+              className="h-1/4 rounded-md p-4"
               style={{
                 background:
                   "linear-gradient(90deg, rgba(2,22,79,255) 0%, #02306b 100%)",
@@ -70,15 +156,191 @@ const IoclMainPage = () => {
             >
               Sensor 3
             </div>
+            <div
+              className="h-1/4 rounded-md p-4"
+              style={{
+                background:
+                  "linear-gradient(90deg, rgba(2,22,79,255) 0%, #02306b 100%)",
+              }}
+            >
+              Last Update
+            </div>
           </div>
 
           {/* graphs and table */}
-          <div className="w-[80%] border border-black p-2 flex flex-col gap-2">
+          <div className="w-[80%]  p-2 flex flex-col gap-2">
             {/* line graph */}
-            <div className="h-1/2 border border-black bg-white"> Line graph</div>
-            <div className="h-1/2 flex gap-2 border border-black">
-              <div className="w-1/2 border border-black bg-white"> table </div>
-              <div className="w-1/2 border border-black bg-white"> bar chart </div>
+            <div className="h-[60%]  bg-white flex flex-col rounded-md">
+              <div className="flex justify-between">
+                <div
+                  className="py-1 px-2 flex flex-1 text-white rounded-tl-md"
+                  style={{
+                    background:
+                      "linear-gradient(90deg, rgba(2,22,79,255) 0%, #02306b 100%)",
+                  }}
+                >
+                  Line Representation of Temperature
+                </div>
+                <div
+                  className=" py-1 px-2  text-[#08174e] rounded-tr-md"
+                  style={{
+                    background:
+                      "linear-gradient(90deg, #f47424 0%, #f58a47 100%)",
+                  }}
+                >
+                  <LiaChartLineSolid size={20} />
+                </div>
+              </div>
+              <div className="flex flex-1  rounded-b-md px-2">
+                <Line data={lineData} options={lineOptions} width={"100%"} />
+              </div>
+            </div>
+            <div className="h-[40%] flex gap-2 ">
+              {/* table */}
+              <div
+                className="w-1/2  bg-white overflow-auto rounded-md"
+                style={{ scrollbarWidth: "none" }}
+              >
+                <table className=" w-full text-center">
+                  <thead
+                    className="sticky top-0 text-white"
+                    style={{
+                      background:
+                        "linear-gradient(90deg, rgba(2,22,79,255) 0%, #02306b 100%)",
+                    }}
+                  >
+                    <tr className="">
+                      <th className="w-1/5 p-1">S.No</th>
+                      <th className="w-1/5 p-1">Sensor 1</th>
+                      <th className="w-1/5 p-1">Sensor 2</th>
+                      <th className="w-1/5 p-1">Sensor 3</th>
+                      <th className="w-1/5 p-1">Last&nbsp;Updated</th>
+                    </tr>
+                  </thead>
+
+                  <tbody className="text-sm">
+                    <tr className=" ">
+                      <td>1</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                    <tr className=" ">
+                      <td>2</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                    <tr className=" ">
+                      <td>3</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                    <tr className=" ">
+                      <td>4</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                    <tr className=" ">
+                      <td>5</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                    <tr className=" ">
+                      <td>6</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                    <tr className=" ">
+                      <td>7</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                    <tr className=" ">
+                      <td>8</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                    <tr className=" ">
+                      <td>9</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                    <tr className=" ">
+                      <td>10</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                    <tr className=" ">
+                      <td>11</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                    <tr className=" ">
+                      <td>12</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                    <tr className=" ">
+                      <td>13</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                    <tr className=" ">
+                      <td>14</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                    <tr className=" ">
+                      <td>15</td>
+                      <td>55</td>
+                      <td>65</td>
+                      <td>75</td>
+                      <td>1</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="w-1/2  bg-white rounded-md">
+                <div
+                  className="py-1 px-2 text-white rounded-t-md"
+                  style={{
+                    background:
+                      "linear-gradient(90deg, rgba(2,22,79,255) 0%, #02306b 100%)",
+                  }}
+                >
+                  Peak Analysis
+                </div>
+                <div className="flex flex-1">
+                  <Bar data={barData} options={barOptions} width={"100%"} />
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -88,3 +350,4 @@ const IoclMainPage = () => {
 }
 
 export default IoclMainPage
+   
--- src/Component/Iocl/IoclSidebar.jsx
+++ src/Component/Iocl/IoclSidebar.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import ioclLogo from "../Assets/iocl2.png";
-import xymaLogo from "../Assets/xyma.png";
+import ioclLogo from "../Assets/ioclRound.png";
+import xymaLogo from "../Assets/xyma - Copy.png";
 import { AiOutlineDashboard } from "react-icons/ai";
 import { PiChartLineUp } from "react-icons/pi";
 import { TbReportSearch } from "react-icons/tb";
@@ -11,107 +11,191 @@ import { FaAnglesRight } from "react-icons/fa6";
 
 const IoclSidebar = () => {
 
-  const [dashboardEnter, setDashboardEnter] = useState(false);
-  const [graphsEnter, setGraphsEnter] = useState(false);
-  const [reportsEnter, setReportsEnter] = useState(false);
-  const [settingsEnter, setSettingsEnter] = useState(false);
-
   const location = useLocation();
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1024);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 1024);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-    <div className="border border-black h-full">
-      <div className="min-h-[25%] border border-black p-4 flex justify-center items-center">
-        <img
-          src={ioclLogo}
-          className="max-w-[100px] object-cover"
-          alt="ioclLogo"
-        />
-      </div>
-      <div className="h-[55%] border border-black font-medium text-gray-800">
-        <Link to="/">
+    <>
+      {isLargeScreen ? (
+        <>
           <div
-            className={`border border-black h-1/4 flex items-center gap-2 p-2 hover:bg-white duration-200 ${
-              location.pathname === "/" ? "bg-white" : ""
-            }`}
-            onMouseEnter={() => setDashboardEnter(true)}
-            onMouseLeave={() => setDashboardEnter(false)}
+            className=" h-full text-white"
+            style={{
+              background:
+                "linear-gradient(90deg, rgba(2,22,79,255) 0%, #02306b 100%)",
+            }}
           >
-            {(dashboardEnter || location.pathname === "/") && (
-              <div className="text-[#f3741c]">
-                <FaAnglesRight size={20} />
-              </div>
-            )}
-            <div>
-              <AiOutlineDashboard size={25} />
+            <div className="h-[25%]  p-4 flex justify-center items-center">
+              <img
+                src={ioclLogo}
+                className="max-w-[100px] object-cover"
+                alt="ioclLogo"
+              />
             </div>
-            <div>Dashboard</div>
-          </div>
-        </Link>
-        <Link to="/ioclGraphs">
-          <div
-            className={`border border-black h-1/4 flex items-center gap-2 p-2 hover:bg-white ${
-              location.pathname === "/ioclGraphs" ? "bg-white" : ""
-            } duration-200`}
-            onMouseEnter={() => setGraphsEnter(true)}
-            onMouseLeave={() => setGraphsEnter(false)}
-          >
-            {(graphsEnter || location.pathname === "/ioclGraphs") && (
-              <div className="text-[#f3741c]">
-                <FaAnglesRight size={20} />
-              </div>
-            )}
-            <div>
-              <PiChartLineUp size={25} />
+            <div className="h-[55%]  font-medium p-4 flex flex-col justify-around">
+              <Link to="/">
+                <div
+                  className={`  flex items-center gap-2 py-4 px-2 rounded-lg hover:bg-white hover:text-[#08174e] duration-200 ${
+                    location.pathname === "/" ? "bg-white text-[#08174e]" : ""
+                  }`}
+                >
+                  {location.pathname === "/" && (
+                    <div className="text-[#08174e]">
+                      <FaAnglesRight size={20} />
+                    </div>
+                  )}
+                  <div>
+                    <AiOutlineDashboard size={25} />
+                  </div>
+                  <div>Dashboard</div>
+                </div>
+              </Link>
+              <Link to="/ioclGraphs">
+                <div
+                  className={`  flex items-center gap-2 py-4 px-2 rounded-lg hover:bg-white hover:text-[#08174e] ${
+                    location.pathname === "/ioclGraphs"
+                      ? "bg-white text-[#08174e]"
+                      : ""
+                  } duration-200`}
+                >
+                  {location.pathname === "/ioclGraphs" && (
+                    <div className="text-[#08174e]">
+                      <FaAnglesRight size={20} />
+                    </div>
+                  )}
+                  <div>
+                    <PiChartLineUp size={25} />
+                  </div>
+                  <div>Graphs</div>
+                </div>
+              </Link>
+              <Link to="/ioclReports">
+                <div
+                  className={` flex items-center gap-2 py-4 px-2 rounded-lg hover:bg-white hover:text-[#08174e] duration-200 ${
+                    location.pathname === "/ioclReports"
+                      ? "bg-white text-[#08174e]"
+                      : ""
+                  }`}
+                >
+                  {location.pathname === "/ioclReports" && (
+                    <div className="text-[#08174e]">
+                      <FaAnglesRight size={20} />
+                    </div>
+                  )}
+                  <div>
+                    <TbReportSearch size={25} />
+                  </div>
+                  <div>Reports</div>
+                </div>
+              </Link>
+              <Link to="/ioclSettings">
+                <div
+                  className={`  flex items-center gap-2 py-4 px-2 rounded-lg hover:bg-white hover:text-[#08174e] duration-200 ${
+                    location.pathname === "/ioclSettings"
+                      ? "bg-white text-[#08174e]"
+                      : ""
+                  }`}
+                >
+                  {location.pathname === "/ioclSettings" && (
+                    <div className="text-[#08174e]">
+                      <FaAnglesRight size={20} />
+                    </div>
+                  )}
+                  <div>
+                    <MdOutlineSettings size={25} />
+                  </div>
+                  <div>Settings</div>
+                </div>
+              </Link>
             </div>
-            <div>Graphs</div>
-          </div>
-        </Link>
-        <Link to="/ioclReports">
-          <div
-            className={`border border-black h-1/4 flex items-center gap-2 p-2 hover:bg-white duration-200 ${
-              location.pathname === "/ioclReports" ? "bg-white" : ""
-            }`} 
-            onMouseEnter={() => setReportsEnter(true)}
-            onMouseLeave={() => setReportsEnter(false)}
-          >
-            {(reportsEnter || location.pathname === "/ioclReports") && (
-              <div className="text-[#f3741c]">
-                <FaAnglesRight size={20} />
+            <div className="flex flex-col items-center justify-center gap-2 min-h-[20%]  p-4">
+              <div className="text-sm text-center">
+                {" "}
+                &copy; All Rights Reserved By
               </div>
-            )}
-            <div>
-              <TbReportSearch size={25} />
-            </div>
-            <div>Reports</div>
-          </div>
-        </Link>
-        <Link to="/ioclSettings">
-          <div
-            className={`border border-black h-1/4 flex items-center gap-2 p-2 hover:bg-white duration-200 ${
-              location.pathname === "/ioclSettings" ? "bg-white" : ""
-            }`}
-            onMouseEnter={() => setSettingsEnter(true)}
-            onMouseLeave={() => setSettingsEnter(false)}
-          >
-            {(settingsEnter || location.pathname === "/ioclSettings") && (
-              <div className="text-[#f3741c]">
-                <FaAnglesRight size={20} />
+              <div>
+                <img src={xymaLogo} className="max-w-[100px]" alt="xymaLogo" />
               </div>
-            )}
-            <div>
-              <MdOutlineSettings size={25} />
             </div>
-            <div>Settings</div>
           </div>
-        </Link>
-      </div>
-      <div className="flex flex-col items-center justify-center gap-2 min-h-[20%] border border-black p-4">
-        <div className="text-sm text-center">All Rights Reserved By</div>
-        <div>
-          <img src={xymaLogo} className="max-w-[100px]" alt="xymaLogo" />
+        </>
+      ) : (
+        <div
+          className="bg-white w-full fixed bottom-0 text-white p-2 flex justify-evenly"
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(2,22,79,255) 0%, #02306b 100%)",
+          }}
+        >
+          <Link to="/">
+            <div
+              className={`  flex flex-col items-center gap-1 p-1 rounded-lg hover:bg-white hover:text-[#08174e] duration-200 ${
+                location.pathname === "/" ? "bg-white text-[#08174e]" : ""
+              }`}
+            >
+              <div>
+                <AiOutlineDashboard size={25} />
+              </div>
+              {/* <div className="text-[10px]">Dashboard</div> */}
+            </div>
+          </Link>
+          <Link to="/ioclGraphs">
+            <div
+              className={` flex flex-col items-center gap-1 p-1 rounded-lg hover:bg-white hover:text-[#08174e] ${
+                location.pathname === "/ioclGraphs"
+                  ? "bg-white text-[#08174e]"
+                  : ""
+              } duration-200`}
+            >
+              <div>
+                <PiChartLineUp size={25} />
+              </div>
+              {/* <div className="text-[10px]">Graphs</div> */}
+            </div>
+          </Link>
+          <Link to="/ioclReports">
+            <div
+              className={` flex flex-col items-center gap-1 p-1 rounded-lg hover:bg-white hover:text-[#08174e] duration-200 ${
+                location.pathname === "/ioclReports"
+                  ? "bg-white text-[#08174e]"
+                  : ""
+              }`}
+            >
+              <div>
+                <TbReportSearch size={25} />
+              </div>
+              {/* <div className="text-[10px]">Reports</div> */}
+            </div>
+          </Link>
+          <Link to="/ioclSettings">
+            <div
+              className={`  flex flex-col items-center gap-1 p-1 rounded-lg hover:bg-white hover:text-[#08174e] duration-200 ${
+                location.pathname === "/ioclSettings"
+                  ? "bg-white text-[#08174e]"
+                  : ""
+              }`}
+            >
+              <div>
+                <MdOutlineSettings size={25} />
+              </div>
+              {/* <div className="text-[10px]">Settings</div> */}
+            </div>
+          </Link>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
